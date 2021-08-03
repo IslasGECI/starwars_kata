4d5a016c5488001c8508d713071da6c0244fa880
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-08-03 15:44:23 -0700

    🧪 Add failing tests

--- tests/test_swapi.sh
+++ tests/test_swapi.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
-curl https://swapi.dev/api/people/1 --output obtained.json
-jq --argfile expected_response tests/expected.json --argfile obtained_response obtained.json -n '$expected_response == $obtained_response'
+#
+# Tests the SWAPI API
+
+# Test the people endpoint
+endpoint="people/1/"
+curl https://swapi.dev/api/"${endpoint}" --output obtained.json
+jq --argfile expected_response tests/expected-people.json --argfile obtained_response obtained.json -n '$expected_response == $obtained_response'
 jsonschema --instance obtained.json tests/swapi-people-schema.json
+
+# Test the planets endpoint
+endpoint="planets/3/"
+curl https://swapi.dev/api/"${endpoint}" --output obtained.json
+jq --argfile expected_response tests/expected-planets.json --argfile obtained_response obtained.json -n '$expected_response == $obtained_response'
+jsonschema --instance obtained.json tests/swapi-planets-schema.json
+
+# Test the starships endpoint
+endpoint="starships/9/"
+curl https://swapi.dev/api/"${endpoint}" --output obtained.json
+jq --argfile expected_response tests/expected-starships.json --argfile obtained_response obtained.json -n '$expected_response == $obtained_response'
+jsonschema --instance obtained.json tests/swapi-starships-schema.json
