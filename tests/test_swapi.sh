f697c89aed4b856efca8ee8b621b54ae5dc6174c
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-08-03 18:49:25 -0700

    ♻️ Extract functions

--- tests/test_swapi.sh
+++ tests/test_swapi.sh
@@ -2,20 +2,40 @@
 #
 # Tests the SWAPI API
 
+source src/helper.sh
+export API_URL="https://swapi.dev/api/"
+
 # Test the people endpoint
+# Person 1 is equal to expected person
 endpoint="people/1/"
-curl https://swapi.dev/api/"${endpoint}" --output obtained.json
-jq --argfile expected_response tests/expected-people.json --argfile obtained_response obtained.json -n '$expected_response == $obtained_response'
-jsonschema --instance obtained.json tests/swapi-people-schema.json
+expected_json="tests/expected-people.json"
+assert_equal "${endpoint}" "${expected_json}"
+
+# Person 2 is valid
+endpoint="people/2/"
+schema="tests/swapi-people-schema.json"
+validate_json "${endpoint}" "${schema}"
 
 # Test the planets endpoint
+# Planet 3 is equal to expected planet
 endpoint="planets/3/"
-curl https://swapi.dev/api/"${endpoint}" --output obtained.json
-jq --argfile expected_response tests/expected-planets.json --argfile obtained_response obtained.json -n '$expected_response == $obtained_response'
-jsonschema --instance obtained.json tests/swapi-planets-schema.json
+expected_json="tests/expected-planets.json"
+assert_equal "${endpoint}" "${expected_json}"
+
+# Planet 2 is valid
+endpoint="planets/2/"
+schema="tests/swapi-planets-schema.json"
+validate_json "${endpoint}" "${schema}"
+
 
 # Test the starships endpoint
+# Starship 9 is equal to expected starship
 endpoint="starships/9/"
-curl https://swapi.dev/api/"${endpoint}" --output obtained.json
-jq --argfile expected_response tests/expected-starships.json --argfile obtained_response obtained.json -n '$expected_response == $obtained_response'
-jsonschema --instance obtained.json tests/swapi-starships-schema.json
+expected_json="tests/expected-starships.json"
+assert_equal "${endpoint}" "${expected_json}"
+
+# Starship 2 is valid
+endpoint="starships/2/"
+schema="tests/swapi-starships-schema.json"
+validate_json "${endpoint}" "${schema}"
+
